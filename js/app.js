7b64bfe4a47be01a88a1e131301336994e82a3b6
Mariam Kobuladze <m.kobuladze@gmail.com>
2016-02-12 16:34:51 +0400

    all the responsive styles done

--- js/app.js
+++ js/app.js
@@ -2,11 +2,24 @@
 
 $(function () {
 
-  if ($('#menu a').length) {
-  
-    $('#menu a').click(function () {
-      $('body').toggleClass('menu-active');
-    });
+  if ($('#hamburger a').length) {
+
+ 	$('body').click(function (e) {
+ 	  if (e.target === $('#share > a')[0]) {
+ 	    $('body').toggleClass('share-active');
+ 	  }
+ 	  else if ($('body').hasClass('share-active')) {
+ 	    $('body').removeClass('share-active');
+ 	  }
+ 	  
+
+ 	  if (e.target === $('#hamburger a')[0]) {
+ 	    $('body').toggleClass('menu-active');
+ 	  }
+ 	  else if ($('body').hasClass('menu-active')) {
+ 	    $('body').removeClass('menu-active');
+ 	  }
+ 	});
  
   }
 
